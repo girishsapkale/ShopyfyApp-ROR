3e683d7a8486002ef72369f4668779d0c28c6552
Rohit Malik <rohitmalik@mobikasa.com>
2015-11-20 18:42:32 +0530

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   if Shop.first.nil?
     Shop.create(:url => "https://5000b6fab6a48677813d80080e505c18:1eebe1b232ede28bfa0e68c673a317d7@rormobikasa.myshopify.com/admin")
+    ShopifyAPI::Base.site = Shop.last.url
   else
     shop_url = Shop.last.url
     ShopifyAPI::Base.site = shop_url
