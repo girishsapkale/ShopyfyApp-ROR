fa32272eb19cacc181a49c52bfa950d8bfc69e53
Rohit Malik <rohitmalik@mobikasa.com>
2015-11-20 18:35:15 +0530

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #if Shop.first.nil? || Shop.present?
-    shop_url = "https://5000b6fab6a48677813d80080e505c18:1eebe1b232ede28bfa0e68c673a317d7@rormobikasa.myshopify.com/admin"
-    #Shop.create(:url => "https://5000b6fab6a48677813d80080e505c18:1eebe1b232ede28bfa0e68c673a317d7@rormobikasa.myshopify.com/admin")
-  #else
-    #shop_url = Shop.last.url
+  if Shop.first.nil?
+    Shop.create(:url => "https://5000b6fab6a48677813d80080e505c18:1eebe1b232ede28bfa0e68c673a317d7@rormobikasa.myshopify.com/admin")
+  else
+    shop_url = Shop.last.url
     ShopifyAPI::Base.site = shop_url
-  #end
-  
-
-  
+  end
 end
